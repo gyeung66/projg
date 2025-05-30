52952eb5015ca17c67fc51dae16655dbc897195f
george <gyeung@mac.com>
2025-05-29 21:47:12 -0700

    dj rev comp

--- projg1/projg1_app/urls.py
+++ projg1/projg1_app/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
-from .views import hello
+from .views import hello,revcomp, start
 
-urlpatterns = [path("", hello, name="hello")]
+urlpatterns = [
+    path("", hello, name="hello"),
+    path("start/", start, name="start"),
+    path("revcomp", revcomp, name="revcomp"),
+    ]
--- projg1/projg1_app/views.py
+++ projg1/projg1_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from Bio.Seq import Seq
 
 # Create your views here.
 def hello(request):
@@ -7,3 +8,11 @@ def hello(request):
     """
     return render(request, 'hello.html', {'greeting': 'Hello, World! Again!!'})
 
+def start(request):
+    return render(request, "input.html")
+
+def revcomp(request):
+    seq = Seq(request.POST["sequence"])
+    revcomp = seq.reverse_complement()
+
+    return render(request, "result.html", {"result": revcomp})
